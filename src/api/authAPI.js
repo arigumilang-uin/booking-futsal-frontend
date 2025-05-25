4e7e550f94ad00cb51676244a096d223c2302fc7
Ari Gumilang <12350110349@students.uin-suska.ac.id>
2025-05-25 18:31:53 +0700

    update auth

--- src/api/authAPI.js
+++ src/api/authAPI.js
@@ -2,11 +2,11 @@
 import axiosInstance from './axiosInstance';
 
 export const loginUser = async (email, password) => {
-  const response = await axiosInstance.post('/auth/login', { email, password });
+  const response = await axiosInstance.post('/user/auth/login', { email, password });
   return response.data;
 };
 
 export const registerUser = async (name, email, password) => {
-  const response = await axiosInstance.post('/auth/register', { name, email, password });
+  const response = await axiosInstance.post('/user/auth/register', { name, email, password });
   return response.data;
 };
